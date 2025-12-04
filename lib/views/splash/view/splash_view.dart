001b8cfa2d96919c2efc49aa6f6ccbcb224659f6
Mayssan <mayssanalashkar@gmail.com>
2025-12-04 21:40:06 +0200

    add the splach screen code

---
+++ lib/views/splash/view/splash_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:medication_app_v0/core/base/view/base_widget.dart';
+import 'package:medication_app_v0/core/components/widgets/lottie_widget.dart';
+import 'package:medication_app_v0/core/constants/app_constants/app_constants.dart';
+import 'package:medication_app_v0/core/constants/image/image_constants.dart';
+import 'package:medication_app_v0/core/extention/context_extention.dart';
+import 'package:medication_app_v0/core/init/theme/color_theme.dart';
+import 'package:medication_app_v0/views/splash/viewmodel/splash_viewmodel.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class SplashView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<SplashViewModel>(
+      model: SplashViewModel(),
+      onModelReady: (SplashViewModel model) {
+        model.setContext(context);
+        model.init();
+      },
+      builder: (BuildContext context, SplashViewModel viewModel) =>
+          _buildScaffold(viewModel, context),
+    );
+  }
+
+  Scaffold _buildScaffold(SplashViewModel viewModel, BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          _floatingActionButtonChangeLanguage(viewModel, context),
+          context.emptySizedHeightBoxNormal,
+          //_floatingActionButtonLogin(viewModel),
+        ],
+      ),
+      body: Container(child: buildLottie, color: ColorTheme.GREY_LIGHT),
+    );
+  }
+
+  FloatingActionButton _floatingActionButtonChangeLanguage(
+      SplashViewModel viewModel, BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        viewModel.changeLanguage(context);
+      },
+      child: context.locale == AppConstants.TR_LOCALE
+          ? Image.asset(ImageConstants.instance.trFlag)
+          : Image.asset(ImageConstants.instance.ukFlag),
+      mini: true,
+    );
+  }
+
+  Center get buildLottie {
+    return Center(child: LottieCustomWidget(path: "medical_shield"));
+  }
+}
---
+++ lib/views/splash/viewmodel/splash_viewmodel.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:medication_app_v0/core/base/viewmodel/base_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:medication_app_v0/core/constants/app_constants/app_constants.dart';
+import 'package:medication_app_v0/core/constants/navigation/navigation_constants.dart';
+import 'package:mobx/mobx.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+part 'splash_viewmodel.g.dart';
+
+class SplashViewModel = _SplashViewModelBase with _$SplashViewModel;
+
+abstract class _SplashViewModelBase with Store, BaseViewModel {
+  @observable
+  bool isLoading = false;
+  void setContext(BuildContext context) => this.viewContext = context;
+  void init() {
+    asyncInit();
+  }
+
+  Future<void> asyncInit() async {
+    await Future.delayed(Duration(seconds: 2));
+    await Future.microtask(() => splashLoginButtonOnPress());
+  }
+  void navigateLogin() {
+    navigation.navigateToPageClear(path: NavigationConstants.LOGIN_VIEW);
+  }
+
+  void navigateHome() {
+    navigation.navigateToPageClear(path: NavigationConstants.HOME_VIEW);
+  }
+
+  void changeLanguage(BuildContext context) {
+    if (context.locale.countryCode == AppConstants.EN_LOCALE.countryCode) {
+      context.setLocale(AppConstants.TR_LOCALE);
+    } else if (context.locale.countryCode == AppConstants.TR_LOCALE.countryCode) {
+      context.setLocale(AppConstants.EN_LOCALE);
+    }
+  }
+
+  Future<void> splashLoginButtonOnPress() async {
+    FirebaseAuth.instance.currentUser == null
+        ? navigateLogin()
+        : navigateHome();
+  }
+
+  @action
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+}
